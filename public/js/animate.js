e3d028a4fa64a80e3217e37fcc95b86f6d3ffeab
Olu-Sammy <masei25@gmail.com>
2021-04-19 17:30:20 +0100

    updated

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,5 +1,4 @@
-var banner = document.querySelector(".banner");
-var whatsapp = document.querySelector(".whatsapp");
+const banner = document.querySelector(".banner");
 
 window.onload = function(){ 
     setInterval(function(){
@@ -7,3 +6,22 @@ window.onload = function(){
     }, 2000)
 }
 
+//lazy loading
+const images = document.querySelectorAll(".img");
+
+let imageOption = {};
+
+let observer = new IntersectionObserver((val, observer) => {
+    val.forEach((entry) => {
+        if(!entry.isIntersecting) return;
+        const image = entry.target
+        const imgSrc = image.getAttribute("src");
+        image.src = imgSrc;
+    });
+}, imageOption);
+
+images.forEach((image) => {
+    observer.observe(image);      
+});
+
+ 
